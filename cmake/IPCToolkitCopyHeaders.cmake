acb7664792982685f6de28468ba126f5e531834f
zfergus <zfergus@nyu.edu>
2020-09-19 14:37:37 -0400

    Fixed relative path from src in copy headers

--- cmake/IPCToolkitCopyHeaders.cmake
+++ cmake/IPCToolkitCopyHeaders.cmake
@@ -1,7 +1,7 @@
 # Copy header files into the build directory
 function(ipc_toolkit_copy_headers)
   foreach(filepath IN ITEMS ${ARGN})
-    file(RELATIVE_PATH filename "${CMAKE_SOURCE_DIR}/src" "${filepath}")
+    file(RELATIVE_PATH filename "${IPC_TOOLKIT_SOURCE_DIR}" "${filepath}")
     if(${filename} MATCHES ".*\.(hpp|h|ipp|tpp)$")
       configure_file(${filepath} ${PROJECT_BINARY_DIR}/include/ipc/${filename})
     endif()
