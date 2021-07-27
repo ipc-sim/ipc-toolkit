ceeef71b81af4fc1e7096d1abfcbb25e1fe56a85
Zachary Ferguson <zfergus@nyu.edu>
2021-07-26 23:17:13 -0400

    Updated find python to work with GH actions

--- cmake/recipes/external/pybind11.cmake
+++ cmake/recipes/external/pybind11.cmake
@@ -23,6 +23,19 @@ FetchContent_Declare(
     GIT_SHALLOW TRUE
 )
 
+if (POLICY CMP0094)  # https://cmake.org/cmake/help/latest/policy/CMP0094.html
+    cmake_policy(SET CMP0094 NEW)  # FindPython should return the first matching Python
+endif ()
+
+# needed on GitHub Actions CI: actions/setup-python does not touch registry/frameworks on Windows/macOS
+# this mirrors PythonInterp behavior which did not consult registry/frameworks first
+if (NOT DEFINED Python_FIND_REGISTRY)
+    set(Python_FIND_REGISTRY "LAST")
+endif ()
+if (NOT DEFINED Python_FIND_FRAMEWORK)
+    set(Python_FIND_FRAMEWORK "LAST")
+endif ()
+
 # Pybind11 still uses the deprecated FindPythonInterp. So let's call CMake's
 # new FindPython module and set PYTHON_EXECUTABLE for Pybind11 to pick up.
 # This works well with conda environments.
