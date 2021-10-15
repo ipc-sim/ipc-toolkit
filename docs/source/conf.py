baa7288f2832bed8d8646cf5594be6d8d8ca0b23
Zachary Ferguson <zfergus@nyu.edu>
2021-10-15 14:41:22 -0400

    Added something from Breathe documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,6 +4,31 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys
+import subprocess
+
+
+def run_doxygen(folder):
+    """Run the doxygen make command in the designated folder"""
+    try:
+        retcode = subprocess.call("cd %s; doxygen" % folder, shell=True)
+        if retcode < 0:
+            sys.stderr.write("doxygen terminated by signal %s" % (-retcode))
+    except OSError as e:
+        sys.stderr.write("doxygen execution failed: %s" % e)
+
+
+def generate_doxygen_xml(app):
+    """Run the doxygen make commands if we're on the ReadTheDocs server"""
+    read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+    if read_the_docs_build:
+        run_doxygen("..")
+
+
+def setup(app):
+    # Add hook for building doxygen xml when needed
+    app.connect("builder-inited", generate_doxygen_xml)
+
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
