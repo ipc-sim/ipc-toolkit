5add8a3d0275ac871dbcc6c6af21714465e2a065
Zachary Ferguson <zfergus@nyu.edu>
2022-02-11 16:07:21 -0500

    Converted friction functions to CollisionMesh as param

--- src/friction/friction.hpp
+++ src/friction/friction.hpp
@@ -3,6 +3,7 @@
 #include <Eigen/Core>
 #include <Eigen/Sparse>
 
+#include <ipc/collision_mesh.hpp>
 #include <ipc/collision_constraint.hpp>
 #include <ipc/friction/friction_constraint.hpp>
 #include <ipc/utils/eigen_ext.hpp>
@@ -10,9 +11,8 @@
 namespace ipc {
 
 void construct_friction_constraint_set(
+    const CollisionMesh& mesh,
     const Eigen::MatrixXd& V,
-    const Eigen::MatrixXi& E,
-    const Eigen::MatrixXi& F,
     const Constraints& contact_constraint_set,
     double dhat,
     double barrier_stiffness,
@@ -20,9 +20,8 @@ void construct_friction_constraint_set(
     FrictionConstraints& friction_constraint_set);
 
 void construct_friction_constraint_set(
+    const CollisionMesh& mesh,
     const Eigen::MatrixXd& V,
-    const Eigen::MatrixXi& E,
-    const Eigen::MatrixXi& F,
     const Constraints& contact_constraint_set,
     double dhat,
     double barrier_stiffness,
@@ -30,9 +29,8 @@ void construct_friction_constraint_set(
     FrictionConstraints& friction_constraint_set);
 
 void construct_friction_constraint_set(
+    const CollisionMesh& mesh,
     const Eigen::MatrixXd& V,
-    const Eigen::MatrixXi& E,
-    const Eigen::MatrixXi& F,
     const Constraints& contact_constraint_set,
     double dhat,
     double barrier_stiffness,
@@ -50,36 +48,32 @@ void construct_friction_constraint_set(
 /// @param epsv_times_h
 template <typename T>
 T compute_friction_potential(
+    const CollisionMesh& mesh,
     const Eigen::MatrixXd& V0,
     const Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>& V1,
-    const Eigen::MatrixXi& E,
-    const Eigen::MatrixXi& F,
     const FrictionConstraints& friction_constraint_set,
     double epsv_times_h);
 
 Eigen::VectorXd compute_friction_potential_gradient(
+    const CollisionMesh& mesh,
     const Eigen::MatrixXd& V0,
     const Eigen::MatrixXd& V1,
-    const Eigen::MatrixXi& E,
-    const Eigen::MatrixXi& F,
     const FrictionConstraints& friction_constraint_set,
     double epsv_times_h);
 
 Eigen::SparseMatrix<double> compute_friction_potential_hessian(
+    const CollisionMesh& mesh,
     const Eigen::MatrixXd& V0,
     const Eigen::MatrixXd& V1,
-    const Eigen::MatrixXi& E,
-    const Eigen::MatrixXi& F,
     const FrictionConstraints& friction_constraint_set,
     double epsv_times_h,
     bool project_hessian_to_psd = true);
 
 Eigen::VectorXd compute_friction_force(
+    const CollisionMesh& mesh,
     const Eigen::MatrixXd& X,
     const Eigen::MatrixXd& Ut,
     const Eigen::MatrixXd& U,
-    const Eigen::MatrixXi& E,
-    const Eigen::MatrixXi& F,
     const FrictionConstraints& friction_constraint_set,
     const double dhat,
     const double barrier_stiffness,
@@ -87,10 +81,9 @@ Eigen::VectorXd compute_friction_force(
     const double dmin = 0);
 
 inline Eigen::VectorXd compute_friction_force(
+    const CollisionMesh& mesh,
     const Eigen::MatrixXd& X,
     const Eigen::MatrixXd& U,
-    const Eigen::MatrixXi& E,
-    const Eigen::MatrixXi& F,
     const FrictionConstraints& friction_constraint_set,
     const double dhat,
     const double barrier_stiffness,
@@ -98,16 +91,15 @@ inline Eigen::VectorXd compute_friction_force(
     const double dmin = 0)
 {
     return compute_friction_force(
-        X, Eigen::MatrixXd(), U, E, F, friction_constraint_set, dhat,
+        mesh, X, Eigen::MatrixXd(), U, friction_constraint_set, dhat,
         barrier_stiffness, epsv_times_h, dmin);
 }
 
 Eigen::SparseMatrix<double> compute_friction_force_jacobian(
+    const CollisionMesh& mesh,
     const Eigen::MatrixXd& X,
     const Eigen::MatrixXd& Ut,
     const Eigen::MatrixXd& U,
-    const Eigen::MatrixXi& E,
-    const Eigen::MatrixXi& F,
     const FrictionConstraints& friction_constraint_set,
     const double dhat,
     const double barrier_stiffness,
@@ -116,10 +108,9 @@ Eigen::SparseMatrix<double> compute_friction_force_jacobian(
     const double dmin = 0);
 
 inline Eigen::SparseMatrix<double> compute_friction_force_jacobian(
+    const CollisionMesh& mesh,
     const Eigen::MatrixXd& X,
     const Eigen::MatrixXd& U,
-    const Eigen::MatrixXi& E,
-    const Eigen::MatrixXi& F,
     const FrictionConstraints& friction_constraint_set,
     const double dhat,
     const double barrier_stiffness,
@@ -128,7 +119,7 @@ inline Eigen::SparseMatrix<double> compute_friction_force_jacobian(
     const double dmin = 0)
 {
     return compute_friction_force_jacobian(
-        X, Eigen::MatrixXd(), U, E, F, friction_constraint_set, dhat,
+        mesh, X, Eigen::MatrixXd(), U, friction_constraint_set, dhat,
         barrier_stiffness, epsv_times_h, wrt, dmin);
 }
 
