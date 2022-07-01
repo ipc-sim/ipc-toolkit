465a088347b82c3997efabd4fe1c74641664c00c
Zachary Ferguson <zfergus@nyu.edu>
2022-06-30 21:21:21 -0400

    Update broadphase repo

--- cmake/recipes/sweep_and_tiniest_queue.cmake
+++ cmake/recipes/sweep_and_tiniest_queue.cmake
@@ -15,7 +15,7 @@ else()
     FetchContent_Declare(
         sweep_and_tiniest_queue
         GIT_REPOSITORY https://github.com/dbelgrod/broadphase-gpu.git
-        GIT_TAG 6b8edb7a85fc8aec5d4a7846632eb40dad7e4e74
+        GIT_TAG 8ea831260de6c0100b597f7568434cd4e6eac580
         GIT_SHALLOW FALSE
     )
     FetchContent_MakeAvailable(sweep_and_tiniest_queue)
