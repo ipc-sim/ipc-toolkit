9e0a78f1c781d37d7c229a5a4c29e1a245db444c
Zachary Ferguson <zfergus@nyu.edu>
2022-08-23 15:03:19 -0400

    Removed const double& params

--- src/friction/smooth_friction_mollifier.hpp
+++ src/friction/smooth_friction_mollifier.hpp
@@ -3,7 +3,7 @@
 namespace ipc {
 
 // C1 clamping
-template <typename T> inline T f0_SF(const T& x, const double& epsv_times_h)
+template <typename T> inline T f0_SF(const T& x, const double epsv_times_h)
 {
     assert(epsv_times_h > 0);
     if (abs(x) >= epsv_times_h) {
@@ -15,7 +15,7 @@ template <typename T> inline T f0_SF(const T& x, const double& epsv_times_h)
 
 /// Derivative of f0_SF divided by x
 template <typename T>
-inline T f1_SF_over_x(const T& x, const double& epsv_times_h)
+inline T f1_SF_over_x(const T& x, const double epsv_times_h)
 {
     assert(epsv_times_h > 0);
     if (abs(x) >= epsv_times_h) {
@@ -26,7 +26,7 @@ inline T f1_SF_over_x(const T& x, const double& epsv_times_h)
 
 /// \f$\frac{f_1'(x)x + f_1(x)}{x^3}\f$
 template <typename T>
-inline T df1_x_minus_f1_over_x3(const T& x, const double& epsv_times_h)
+inline T df1_x_minus_f1_over_x3(const T& x, const double epsv_times_h)
 {
     assert(epsv_times_h > 0);
     if (abs(x) >= epsv_times_h) {
