25b94bc7ff42e103308377a4860912c931f6b4e8
Daniele Panozzo <daniele.panozzo@gmail.com>
2022-11-28 12:45:38 -0500

    Fixes problematic casts to/from Eigen

--- python/src/candidates/edge_vertex.cpp
+++ python/src/candidates/edge_vertex.cpp
@@ -1,7 +1,4 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include <pybind11/iostream.h>
-#include <pybind11/operators.h>
+#include "../common.hpp"
 
 #include <ipc/candidates/edge_vertex.hpp>
 
