01637759f2252cc1fb3a88ba0b272fd3c3f403c3
Zachary Ferguson <zfergus@nyu.edu>
2023-03-13 20:51:54 -0400

    Add missing bindings.hpp

---
+++ python/src/ccd/bindings.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <pybind11/pybind11.h>
+namespace py = pybind11;
+
+void define_ccd_aabb(py::module_& m);
+void define_ccd(py::module_& m);
+void define_inexact_point_edge(py::module_& m);
+void define_point_static_plane(py::module_& m);
