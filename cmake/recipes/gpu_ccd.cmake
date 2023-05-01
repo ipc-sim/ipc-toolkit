d17bcfcee51c270a01fe7f81a6e18719557d41ca
dbelgrod <davidbelgrod@gmail.com>
2023-05-01 13:25:48 -0400

    using main ccd-gpu

--- cmake/recipes/gpu_ccd.cmake
+++ cmake/recipes/gpu_ccd.cmake
@@ -4,14 +4,14 @@ endif()
 
 message(STATUS "Third-party: creating target 'gpu_ccd::gpu_ccd'")
 
-option(STQ_WITH_CPU  "Enable CPU Implementation"  ON)
+option(STQ_WITH_CPU "Enable CPU Implementation" ON)
 set(STQ_WITH_CUDA ON CACHE BOOL "Enable CUDA Implementation" FORCE)
 
 include(FetchContent)
 FetchContent_Declare(
     gpu_ccd
     GIT_REPOSITORY https://github.com/dbelgrod/CCD-GPU.git
-    GIT_TAG feb54c029a5fb7b8dd195b5485c3f9e85324199b
+    GIT_TAG main
     GIT_SHALLOW FALSE
 )
 FetchContent_MakeAvailable(gpu_ccd)
