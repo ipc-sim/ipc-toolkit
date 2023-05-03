90c7d9f562fcf0ba5253e4d317af01ef2369c79e
dbelgrod <davidbelgrod@gmail.com>
2023-05-03 18:02:51 -0400

    using cmakelists.txt from broadmark repo

--- cmake/recipes/broadmark.cmake
+++ cmake/recipes/broadmark.cmake
@@ -18,20 +18,20 @@ if(NOT broadmark_POPULATED)
     add_subdirectory(${broadmark_SOURCE_DIR} ${broadmark_BINARY_DIR})
 endif()
 
-file(GLOB BROADMARK_SOURCES
-    "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/*.cpp"
-    "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/*.cpp"
-    "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet3/*.cpp"
-    "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet3/Bullet3Common/*.cpp"
-    "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet3/Bullet3Collision/BroadPhaseCollision/*.cpp"
-    "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet2/*.cpp"
-    "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet2/LinearMath/*.cpp"
-    "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet2/BulletCollision/*.cpp"
-    "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet2/BulletDynamics/*.cpp"
-)
+# file(GLOB BROADMARK_SOURCES
+# "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/*.cpp"
+# "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/*.cpp"
+# "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet3/*.cpp"
+# "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet3/Bullet3Common/*.cpp"
+# "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet3/Bullet3Collision/BroadPhaseCollision/*.cpp"
+# "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet2/*.cpp"
+# "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet2/LinearMath/*.cpp"
+# "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet2/BulletCollision/*.cpp"
+# "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies/Bullet2/BulletDynamics/*.cpp"
+# )
 
-# Create an imported target for the Broadmark library
-add_library(broadmark STATIC ${BROADMARK_SOURCES})
+# # Create an imported target for the Broadmark library
+# add_library(broadmark STATIC ${BROADMARK_SOURCES})
 target_include_directories(broadmark PUBLIC
     "${broadmark_SOURCE_DIR}/Broadmark/Algorithms"
     "${broadmark_SOURCE_DIR}/Broadmark/Algorithms/Dependencies" # Add Bullet3 include directory
