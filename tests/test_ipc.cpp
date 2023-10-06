96bc857d563854ade495cb9f2db169f65db0f3b1
Zachary Ferguson <zfergus@nyu.edu>
2023-10-05 22:32:12 -0400

    Cover is_step_collision_free

--- tests/test_ipc.cpp
+++ tests/test_ipc.cpp
@@ -430,3 +430,16 @@ TEST_CASE("Convergent formulation", "[ipc][convergent]")
 
     CHECK(fd::compare_gradient(grad_b, fgrad_b));
 }
+
+TEST_CASE("Is step collision free", "[is_step_collision_free]")
+{
+    Eigen::MatrixXd V0, V1;
+    Eigen::MatrixXi E, F;
+    REQUIRE(load_mesh("two-cubes-close.obj", V0, E, F));
+    REQUIRE(load_mesh("two-cubes-intersecting.obj", V1, E, F));
+
+    CollisionMesh mesh(V0, E, F);
+
+    CHECK(is_step_collision_free(mesh, V0, V0));
+    CHECK(!is_step_collision_free(mesh, V0, V1));
+}
