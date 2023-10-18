a0a8e9278e06fd013b847dd9e39c9ef3096d45ca
Zachary Ferguson <zfergus@nyu.edu>
2023-10-18 17:45:16 -0400

    Check distance when no motion in CCD

--- src/ipc/ccd/inexact_point_edge.hpp
+++ src/ipc/ccd/inexact_point_edge.hpp
@@ -1,3 +1,10 @@
+//
+// NOTE: This method is provided for reference comparison and is not utilized by
+// the high-level functionality. In compairson to Tight Inclusion CCD, this CCD
+// method is not provably conservative and so can potentially produce false
+// negatives (i.e., miss collisions) due to floating-point rounding error.
+//
+
 #pragma once
 
 #include <Eigen/Core>
