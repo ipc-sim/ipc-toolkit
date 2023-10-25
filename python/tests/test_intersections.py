219c77965d554e3e22f948d9e33f4fd2a04505c9
Zachary Ferguson <zfergus@nyu.edu>
2023-10-25 15:16:43 -0400

    Wrap segment_segment_intersect

---
+++ python/tests/test_intersections.py
@@ -0,0 +1,7 @@
+import numpy as np
+import ipctk
+
+
+def test_segment_segment_intersect():
+    assert ipctk.segment_segment_intersect(
+        np.array([-1, 0]), np.array([1, 0]), np.array([0, -1]), np.array([0, 1]))
