9ebf6724344fa2bdab86c63e96ac2e42adbc8452
Zachary Ferguson <zfergus@nyu.edu>
2023-10-31 14:11:06 -0400

    Fix STQ bindings

--- python/src/broad_phase/sweep_and_tiniest_queue.cpp
+++ python/src/broad_phase/sweep_and_tiniest_queue.cpp
@@ -9,8 +9,11 @@ void define_sweep_and_tiniest_queue(py::module_& m)
 {
     py::class_<CopyMeshBroadPhase, BroadPhase>(m, "CopyMeshBroadPhase");
 
-#ifdef IPC_TOOLKIT_WITH_CUDA
     py::class_<SweepAndTiniestQueue, CopyMeshBroadPhase>(
         m, "SweepAndTiniestQueue");
+
+#ifdef IPC_TOOLKIT_WITH_CUDA
+    py::class_<SweepAndTiniestQueueGPU, CopyMeshBroadPhase>(
+        m, "SweepAndTiniestQueueGPU");
 #endif
 }
