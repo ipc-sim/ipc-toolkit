fd67ace8f8edb38ac80771df05bce0fa9abda88b
Huangzizhou <zizhou@nyu.edu>
2024-01-08 21:28:21 -0500

    parallelize init_vertex_contact_distance_map

--- src/ipc/collision_mesh.cpp
+++ src/ipc/collision_mesh.cpp
@@ -9,6 +9,9 @@
 #include <ipc/utils/local_to_global.hpp>
 #include <ipc/utils/area_gradient.hpp>
 
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+
 namespace ipc {
 
 CollisionMesh::CollisionMesh(
@@ -381,24 +384,25 @@ void CollisionMesh::init_vertex_contact_distance_map()
 {
     if (dim() == 2) {
         m_vertex_to_rest_config_contact_dist.resize(m_rest_positions.rows(), 1);
-        for (int i = 0; i < m_rest_positions.rows(); i++) {
-            double min_dist_sqr = __DBL_MAX__;
-            for (int j = 0; j < m_edges.rows(); j++) {
-                if (m_edges(j, 0) != i && m_edges(j, 1) != i) {
+        tbb::parallel_for(
+            tbb::blocked_range<size_t>(size_t(0), m_rest_positions.rows()),
+            [&](const tbb::blocked_range<size_t>& r) {
+                for (int i = r.begin(); i < r.end(); i++) {
+                    double min_dist_sqr = std::numeric_limits<double>::max();
+                    for (int j = 0; j < m_edges.rows(); j++) {
+                        if (m_edges(j, 0) != i && m_edges(j, 1) != i) {
+                            const double dist_sqr = point_edge_distance(m_rest_positions.row(i), m_rest_positions.row(m_edges(j, 0)), m_rest_positions.row(m_edges(j, 1)));
 
-                    const VectorMax3d p = m_rest_positions.row(i);
-                    const VectorMax3d e0 = m_rest_positions.row(m_edges(j, 0));
-                    const VectorMax3d e1 = m_rest_positions.row(m_edges(j, 1));
-
-                    const double dist_sqr = point_edge_distance(p, e0, e1);
-
-                    min_dist_sqr = std::min(dist_sqr, min_dist_sqr);
+                            min_dist_sqr = std::min(dist_sqr, min_dist_sqr);
+                        }
+                    }
+                    m_vertex_to_rest_config_contact_dist(i) = min_dist_sqr;
                 }
-            }
-            m_vertex_to_rest_config_contact_dist(i) = min_dist_sqr;
-        }
+            });
+        logger().debug("min distance {}, max distance {}", m_vertex_to_rest_config_contact_dist.minCoeff(), m_vertex_to_rest_config_contact_dist.maxCoeff());
     }
     else if (dim() == 3) {
+        logger().error("3D adaptive dhat not implemented!");
         //TODO
     }
 }
