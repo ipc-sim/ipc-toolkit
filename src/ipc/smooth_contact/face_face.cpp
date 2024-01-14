a47399f5856b5d8291398cd87498c308ba95c525
Huangzizhou <zizhou@nyu.edu>
2024-01-14 15:22:11 -0500

    fix compile error

--- src/ipc/smooth_contact/face_face.cpp
+++ src/ipc/smooth_contact/face_face.cpp
@@ -9,46 +9,46 @@
 // std::mutex mut_face;
 
 namespace ipc {
-    namespace {
-    enum class FD_RULE { CENTRAL, LEFT, RIGHT };
+    // namespace {
+    // enum class FD_RULE { CENTRAL, LEFT, RIGHT };
     
-    void my_finite_gradient(const Eigen::VectorXd& x, const std::function<double(const Eigen::VectorXd&)> &f, Eigen::VectorXd &grad, FD_RULE rule = FD_RULE::CENTRAL, const double eps = 1e-7)
-    {
-        grad.setZero(x.size());
-        switch (rule)
-        {
-        case FD_RULE::CENTRAL:
-            for (int i = 0; i < x.size(); i++)
-                for (int d : {-1, 1})
-                {
-                    auto y = x;
-                    y(i) += d * eps;
-                    grad(i) += d * f(y) / (2*eps);
-                }
-            break;
-        case FD_RULE::LEFT:
-            for (int i = 0; i < x.size(); i++)
-            {
-                    auto y = x;
-                    grad(i) += f(y) / eps;
-                    y(i) -= eps;
-                    grad(i) -= f(y) / eps;
-            }
-            break;
-        case FD_RULE::RIGHT:
-            for (int i = 0; i < x.size(); i++)
-            {
-                    auto y = x;
-                    grad(i) -= f(y) / eps;
-                    y(i) += eps;
-                    grad(i) += f(y) / eps;
-            }
-            break;
-        default:
-        assert(false);
-        }
-    }
-    }
+    // void my_finite_gradient(const Eigen::VectorXd& x, const std::function<double(const Eigen::VectorXd&)> &f, Eigen::VectorXd &grad, FD_RULE rule = FD_RULE::CENTRAL, const double eps = 1e-7)
+    // {
+    //     grad.setZero(x.size());
+    //     switch (rule)
+    //     {
+    //     case FD_RULE::CENTRAL:
+    //         for (int i = 0; i < x.size(); i++)
+    //             for (int d : {-1, 1})
+    //             {
+    //                 auto y = x;
+    //                 y(i) += d * eps;
+    //                 grad(i) += d * f(y) / (2*eps);
+    //             }
+    //         break;
+    //     case FD_RULE::LEFT:
+    //         for (int i = 0; i < x.size(); i++)
+    //         {
+    //                 auto y = x;
+    //                 grad(i) += f(y) / eps;
+    //                 y(i) -= eps;
+    //                 grad(i) -= f(y) / eps;
+    //         }
+    //         break;
+    //     case FD_RULE::RIGHT:
+    //         for (int i = 0; i < x.size(); i++)
+    //         {
+    //                 auto y = x;
+    //                 grad(i) -= f(y) / eps;
+    //                 y(i) += eps;
+    //                 grad(i) += f(y) / eps;
+    //         }
+    //         break;
+    //     default:
+    //     assert(false);
+    //     }
+    // }
+    // }
     std::array<long, 8> SmoothFaceFaceCollision::vertex_ids(
         const Eigen::MatrixXi& edges, const Eigen::MatrixXi& faces) const
     {
