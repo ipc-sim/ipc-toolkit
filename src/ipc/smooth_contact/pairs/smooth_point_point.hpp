9744902db94e14979d54a9b975dfab394fa195e3
Huangzizhou <zizhou@nyu.edu>
2024-02-03 15:16:50 -0500

    remove not used functions

--- src/ipc/smooth_contact/pairs/smooth_point_point.hpp
+++ src/ipc/smooth_contact/pairs/smooth_point_point.hpp
@@ -33,23 +33,4 @@ scalar smooth_point_point_potential_2d(
             smooth_point2_term<scalar>(vb,  direc, eb0, eb1, params.alpha, params.beta);
 }
 
-template <typename scalar>
-scalar smooth_point_point_potential_3d(
-    const Eigen::Ref<const RowVector3<scalar>>& va,
-    const Eigen::Ref<const RowVector3<scalar>>& vb,
-    const Eigen::Ref<const Eigen::Matrix<scalar, -1, 3>>& ra,
-    const Eigen::Ref<const Eigen::Matrix<scalar, -1, 3>>& rb,
-    const ParameterType &params,
-    const std::array<ORIENTATION_TYPES, 2> &otypes)
-{
-    RowVector3<scalar> direc = va - vb;
-    const scalar dist = direc.norm();
-    direc = direc / dist;
-
-    const scalar term_a = smooth_point3_term<scalar>(va, direc, ra, params.alpha, params.beta, otypes[0]);
-    const scalar term_b = smooth_point3_term<scalar>(vb, -direc, rb, params.alpha, params.beta, otypes[1]);
-    const scalar barrier = Math<scalar>::inv_barrier(dist / params.dhat, params.r);
-
-    return term_a * term_b * barrier;
-}
 }
