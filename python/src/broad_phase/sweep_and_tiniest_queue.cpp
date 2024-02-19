aae74993b24f777ffa6f28af52e6fba6316145a5
Zachary Ferguson <zy.fergus@gmail.com>
2024-02-19 11:51:16 -0500

    Fix namespace issue in Python bindings

--- python/src/broad_phase/sweep_and_tiniest_queue.cpp
+++ python/src/broad_phase/sweep_and_tiniest_queue.cpp
@@ -3,7 +3,9 @@
 #include <ipc/broad_phase/sweep_and_tiniest_queue.hpp>
 
 namespace py = pybind11;
-using namespace ipc;
+#ifdef IPC_TOOLKIT_WITH_CUDA
+using namespace ipc; // not defined if IPC_TOOLKIT_WITH_CUDA is not defined
+#endif
 
 void define_sweep_and_tiniest_queue(py::module_& m)
 {
