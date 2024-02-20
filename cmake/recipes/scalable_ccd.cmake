9f8db1a7653295f70a0f602076242a5050f99131
Zachary Ferguson <zy.fergus@gmail.com>
2024-02-19 19:41:00 -0500

    Update scalable-ccd to c03efa0

--- cmake/recipes/scalable_ccd.cmake
+++ cmake/recipes/scalable_ccd.cmake
@@ -9,4 +9,4 @@ message(STATUS "Third-party: creating target 'scalable_ccd::scalable_ccd'")
 set(SCALABLE_CCD_WITH_CUDA ${IPC_TOOLKIT_WITH_CUDA} CACHE BOOL "Enable CUDA CCD" FORCE)
 
 include(CPM)
-CPMAddPackage("gh:continuous-collision-detection/scalable-ccd#40baf6119f8d7702bafacc3acd4c2b501a6b4e76")
+CPMAddPackage("gh:continuous-collision-detection/scalable-ccd#c03efa095173b44c13d8440bbc859eeaa0ec550b")
