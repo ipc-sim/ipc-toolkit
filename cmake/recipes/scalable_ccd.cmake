3b2378a1d459c2a8f855081abad1ce66bff67914
Zachary Ferguson <zfergus@nyu.edu>
2024-05-30 19:10:34 -0400

    Update tight_inclusion and scalable_ccd

--- cmake/recipes/scalable_ccd.cmake
+++ cmake/recipes/scalable_ccd.cmake
@@ -9,4 +9,4 @@ message(STATUS "Third-party: creating target 'scalable_ccd::scalable_ccd'")
 set(SCALABLE_CCD_WITH_CUDA ${IPC_TOOLKIT_WITH_CUDA} CACHE BOOL "Enable CUDA CCD" FORCE)
 
 include(CPM)
-CPMAddPackage("gh:continuous-collision-detection/scalable-ccd#c03efa095173b44c13d8440bbc859eeaa0ec550b")
+CPMAddPackage("gh:continuous-collision-detection/scalable-ccd#60692d7cbc03390a3a198792a3ff2b869c5da05b")
