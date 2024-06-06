8b3a1b24d920b975bf503586eaf7937f8ceb8b0e
Zachary Ferguson <zfergus@nyu.edu>
2024-06-05 20:03:49 -0400

    Add J matrix to AffineBody class

--- src/ipc/dynamics/affine/affine_body.hpp
+++ src/ipc/dynamics/affine/affine_body.hpp
@@ -18,12 +18,46 @@ struct AffineBody {
     {
     }
 
+    Eigen::SparseMatrix<double> J(const Eigen::MatrixXd& rest_positions) const
+    {
+        std::vector<Eigen::Triplet<double>> triplets;
+
+        for (int i = 0; i < rest_positions.rows(); i++) {
+            for (int j = 0; j < rest_positions.cols(); j++) {
+                triplets.emplace_back(rest_positions.rows() * j + i, j, 1);
+            }
+        }
+
+        // I ⊗ x̄
+        for (int i = 0; i < rest_positions.rows(); i++) {
+            for (int j = 0; j < rest_positions.cols(); j++) {
+                for (int k = 0; k < A.cols(); k++) {
+                    triplets.emplace_back(
+                        i + k * rest_positions.rows(),
+                        j + k * rest_positions.cols() + p.size(),
+                        rest_positions(i, j));
+                }
+            }
+        }
+
+        Eigen::SparseMatrix<double> J(
+            rest_positions.size(), A.size() + p.size());
+        J.setFromTriplets(triplets.begin(), triplets.end());
+        return J;
+    }
+
     Eigen::MatrixXd
     transform_vertices(const Eigen::MatrixXd& rest_positions) const
     {
         // Compute: A x̄ + p
         // transpose because x is row-ordered
         return (rest_positions * A.transpose()).rowwise() + p.transpose();
+    }
+
+    Eigen::SparseMatrix<double>
+    transform_vertices_gradient(const Eigen::MatrixXd& rest_positions) const
+    {
+        return J(rest_positions);
     }
 };
 
