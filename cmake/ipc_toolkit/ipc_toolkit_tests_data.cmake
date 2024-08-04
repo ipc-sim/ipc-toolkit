151d2b27a90ccb062a302c86e6e7b0ff1bc97567
Zachary Ferguson <zfergus@nyu.edu>
2024-08-03 21:02:56 -0400

    Add option to use existing tests data

--- cmake/ipc_toolkit/ipc_toolkit_tests_data.cmake
+++ cmake/ipc_toolkit/ipc_toolkit_tests_data.cmake
@@ -1,3 +1,6 @@
+# ipc-toolkit-tests-data (https://github.com/ipc-sim/ipc-toolkit-tests-data)
+# License: MIT
+
 if(TARGET ipc_toolkit_test_data_download)
     return()
 endif()
@@ -5,18 +8,33 @@ endif()
 include(ExternalProject)
 include(FetchContent)
 
-set(IPC_TOOLKIT_TESTS_DATA_DIR "${PROJECT_SOURCE_DIR}/tests/data/" CACHE PATH "Where should we download the test data?")
+set(IPC_TOOLKIT_TESTS_DATA_DIR "${PROJECT_SOURCE_DIR}/tests/data/" CACHE PATH "Where should we download the tests data?")
+option(IPC_TOOLKIT_USE_EXISTING_TESTS_DATA_DIR "Use an existing test data directory instead of downloading it" OFF)
 
-ExternalProject_Add(
-    ipc_toolkit_test_data_download
-    PREFIX "${FETCHCONTENT_BASE_DIR}/tests/data"
-    SOURCE_DIR ${IPC_TOOLKIT_TESTS_DATA_DIR}
+if(IPC_TOOLKIT_USE_EXISTING_TESTS_DATA_DIR)
+    ExternalProject_Add(
+        ipc_toolkit_test_data_download
+        PREFIX "${FETCHCONTENT_BASE_DIR}/tests/data"
+        SOURCE_DIR ${IPC_TOOLKIT_TESTS_DATA_DIR}
 
-    GIT_REPOSITORY https://github.com/ipc-sim/ipc-toolkit-tests-data.git
-    GIT_TAG a4fe14a4fd689a013ae25b72e50bdccac3bf2d04
+        # NOTE: No download step
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+        LOG_DOWNLOAD ON
+    )
+else()
+    ExternalProject_Add(
+        ipc_toolkit_test_data_download
+        PREFIX "${FETCHCONTENT_BASE_DIR}/tests/data"
+        SOURCE_DIR ${IPC_TOOLKIT_TESTS_DATA_DIR}
 
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    LOG_DOWNLOAD ON
-)
+        GIT_REPOSITORY https://github.com/ipc-sim/ipc-toolkit-tests-data.git
+        GIT_TAG a4fe14a4fd689a013ae25b72e50bdccac3bf2d04
+
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND ""
+        INSTALL_COMMAND ""
+        LOG_DOWNLOAD ON
+    )
+endif()
