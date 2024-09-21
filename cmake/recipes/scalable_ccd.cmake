d5949efb7970c865898d1d754bf97c5b16330dec
Antoine Boucher <antoine.bou13@gmail.com>
2024-09-21 11:48:58 -0400

    Update scalable_ccd.cmake

--- cmake/recipes/scalable_ccd.cmake
+++ cmake/recipes/scalable_ccd.cmake
@@ -9,4 +9,4 @@ message(STATUS "Third-party: creating target 'scalable_ccd::scalable_ccd'")
 set(SCALABLE_CCD_WITH_CUDA ${IPC_TOOLKIT_WITH_CUDA} CACHE BOOL "Enable CUDA CCD" FORCE)
 
 include(CPM)
-CPMAddPackage("gh:continuous-collision-detection/scalable-ccd#318352e6aa3c1008a67c80547c6fa21bf18fb1a5")
+CPMAddPackage("gh:ETSTribology/scalable-ccd#7bc498c312de6d8e5aa144ef08fcb227baeaae9b")
