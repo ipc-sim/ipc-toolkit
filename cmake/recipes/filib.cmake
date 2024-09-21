18608f5476c151d43cd03667b012bf5cd58cc549
Zachary Ferguson <zy.fergus@gmail.com>
2024-09-21 01:37:22 -0400

    Update filib

--- cmake/recipes/filib.cmake
+++ cmake/recipes/filib.cmake
@@ -1,10 +1,13 @@
 # filib (https://github.com/zfergus/filib.git)
-# License: LGPL
+# License: LGPL-2.1
 if(TARGET filib::filib)
   return()
 endif()
 
 message(STATUS "Third-party: creating target 'filib::filib'")
 
+# This has to be set to ON to avoid licensing IPC Toolkit under LGPL
+set(FILIB_BUILD_SHARED_LIB ON CACHE BOOL "Build shared library" FORCE)
+
 include(CPM)
-CPMAddPackage("gh:zfergus/filib#1cd377a7c833a68dc47217829e333f4886c5c46d")
+CPMAddPackage("gh:zfergus/filib#5b9cbc73790585b391da1624c1f1ea52eaaeb5ac")
