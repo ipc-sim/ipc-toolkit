b3adcd063a0679d3ea3d51c07e7355b2c1baff65
Antoine Boucher <antoine.boucher.1@ens.etsmtl.ca>
2024-11-04 10:39:38 -0500

    reverse to original recipe

--- cmake/recipes/scalable_ccd.cmake
+++ cmake/recipes/scalable_ccd.cmake
@@ -9,4 +9,4 @@ message(STATUS "Third-party: creating target 'scalable_ccd::scalable_ccd'")
 set(SCALABLE_CCD_WITH_CUDA ${IPC_TOOLKIT_WITH_CUDA} CACHE BOOL "Enable CUDA CCD" FORCE)
 
 include(CPM)
-CPMAddPackage("gh:ETSTribology/scalable-ccd#7bc498c312de6d8e5aa144ef08fcb227baeaae9b")
+CPMAddPackage("gh:continuous-collision-detection/scalable-ccd#de61cd1dd871a2801f6a3ed018d7d45cb77ec963")
