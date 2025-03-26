3c30dc533b650c709a265c5138fa83ab965ba86b
ZizhouHuang <zizhou@nyu.edu>
2025-03-26 16:45:04 -0400

    comment huge test

--- tests/src/tests/potential/test_new.cpp
+++ tests/src/tests/potential/test_new.cpp
@@ -85,63 +85,63 @@ using namespace ipc;
 //     // std::cout << "hess: " << hess_b.norm() << "\n";
 // }
 
-TEST_CASE(
-    "Smooth barrier potential convergence 3D",
-    "[smooth_potential]")
-{
-    const BroadPhaseMethod method = BroadPhaseMethod::BVH;
-    const bool adaptive_dhat = false;
+// TEST_CASE(
+//     "Smooth barrier potential convergence 3D",
+//     "[smooth_potential]")
+// {
+//     const BroadPhaseMethod method = BroadPhaseMethod::BVH;
+//     const bool adaptive_dhat = false;
 
-    double dhat = -1;
-    std::string mesh_name = "";
-    SECTION("debug")
-    {
-        mesh_name = ("2-cubes.obj");
-        dhat = 0.25;
-    }
+//     double dhat = -1;
+//     std::string mesh_name = "";
+//     SECTION("debug")
+//     {
+//         mesh_name = ("2-cubes.obj");
+//         dhat = 0.25;
+//     }
 
-    double min_dist_ratio = 0.8;
-    Eigen::MatrixXd vertices;
-    Eigen::MatrixXi edges, faces;
-    bool success = tests::load_mesh(mesh_name, vertices, edges, faces);
-    CAPTURE(mesh_name);
-    REQUIRE(success);
+//     double min_dist_ratio = 0.8;
+//     Eigen::MatrixXd vertices;
+//     Eigen::MatrixXi edges, faces;
+//     bool success = tests::load_mesh(mesh_name, vertices, edges, faces);
+//     CAPTURE(mesh_name);
+//     REQUIRE(success);
 
-    CollisionMesh mesh;
-    ParameterType param(dhat, 0.8, 0, 0.1, 0, 2);
-    param.set_adaptive_dhat_ratio(min_dist_ratio);
-    SmoothCollisions<3> collisions;
-    mesh = CollisionMesh(vertices, edges, faces);
-    std::cout << "Mesh Memory " << getCurrentRSS() / (1024.*1024) << "MB\n";
+//     CollisionMesh mesh;
+//     ParameterType param(dhat, 0.8, 0, 0.1, 0, 2);
+//     param.set_adaptive_dhat_ratio(min_dist_ratio);
+//     SmoothCollisions<3> collisions;
+//     mesh = CollisionMesh(vertices, edges, faces);
+//     std::cout << "Mesh Memory " << getCurrentRSS() / (1024.*1024) << "MB\n";
 
-    // ignore self-contact
-    {
-        std::vector<int> vids(vertices.rows(), 0);
-        for (int i = 0; i < vertices.rows(); i++)
-            if (vertices(i, 0) < -1.1)
-                vids[i] = 1;
-        mesh.can_collide = [vids](size_t vi, size_t vj) {
-            return vids[vi] != vids[vj];
-        };
-    }
+//     // ignore self-contact
+//     {
+//         std::vector<int> vids(vertices.rows(), 0);
+//         for (int i = 0; i < vertices.rows(); i++)
+//             if (vertices(i, 0) < -1.1)
+//                 vids[i] = 1;
+//         mesh.can_collide = [vids](size_t vi, size_t vj) {
+//             return vids[vi] != vids[vj];
+//         };
+//     }
 
-    // collisions.compute_adaptive_dhat(mesh, vertices, param, method);
-    collisions.build(mesh, vertices, param, adaptive_dhat, method);
-    std::cout << "Collision set Memory " << getCurrentRSS() / (1024.*1024) << "MB\n";
-    CAPTURE(dhat, method, adaptive_dhat);
-    CHECK(collisions.size() > 0);
-    std::cout << "smooth collision candidate size " << collisions.size() << "\n";
+//     // collisions.compute_adaptive_dhat(mesh, vertices, param, method);
+//     collisions.build(mesh, vertices, param, adaptive_dhat, method);
+//     std::cout << "Collision set Memory " << getCurrentRSS() / (1024.*1024) << "MB" << std::endl;
+//     CAPTURE(dhat, method, adaptive_dhat);
+//     CHECK(collisions.size() > 0);
+//     std::cout << "smooth collision candidate size " << collisions.size() << std::endl;
 
-    CHECK(!has_intersections(mesh, vertices));
+//     CHECK(!has_intersections(mesh, vertices));
 
-    SmoothContactPotential<SmoothCollisions<3>> potential(param);
-    std::cout << std::setprecision(12) << "energy: " << potential(collisions, mesh, vertices) << "\n";
+//     SmoothContactPotential<SmoothCollisions<3>> potential(param);
+//     std::cout << std::setprecision(12) << "energy: " << potential(collisions, mesh, vertices) << std::endl;
 
-    // -------------------------------------------------------------------------
-    // Gradient
-    // -------------------------------------------------------------------------
+//     // -------------------------------------------------------------------------
+//     // Gradient
+//     // -------------------------------------------------------------------------
 
-    // const Eigen::VectorXd grad_b =
-    //     potential.gradient(collisions, mesh, vertices);
-    // std::cout << "grad: " << grad_b.norm() << "\n";
-}
+//     // const Eigen::VectorXd grad_b =
+//     //     potential.gradient(collisions, mesh, vertices);
+//     // std::cout << "grad: " << grad_b.norm() << "\n";
+// }
