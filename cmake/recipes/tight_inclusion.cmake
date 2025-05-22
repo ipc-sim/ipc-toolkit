086abf32f5fc93d3b0e2023b395b939088d397b1
zachferguson <zach.ferguson@clo3d.com>
2025-05-22 09:49:14 -0400

    Update Tight Inclusion package version to 1.0.6

--- cmake/recipes/tight_inclusion.cmake
+++ cmake/recipes/tight_inclusion.cmake
@@ -7,7 +7,7 @@ endif()
 message(STATUS "Third-party: creating target 'tight_inclusion::tight_inclusion'")
 
 include(CPM)
-CPMAddPackage("gh:Continuous-Collision-Detection/Tight-Inclusion@1.0.5")
+CPMAddPackage("gh:Continuous-Collision-Detection/Tight-Inclusion@1.0.6")
 
 # Folder name for IDE
 set_target_properties(tight_inclusion PROPERTIES FOLDER "ThirdParty")
