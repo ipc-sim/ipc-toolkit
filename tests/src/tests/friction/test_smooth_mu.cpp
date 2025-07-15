3c95f20228d4545b87ca6a845941e295acc89d0c
Zachary Ferguson <zy.fergus@gmail.com>
2025-07-15 09:42:42 -0400

    Fix smooth_mu test case with improved FD accuracy

--- tests/src/tests/friction/test_smooth_mu.cpp
+++ tests/src/tests/friction/test_smooth_mu.cpp
@@ -11,39 +11,25 @@ using namespace ipc;
 TEST_CASE("Smooth mu", "[friction][mollifier][mu]")
 {
 
-    static constexpr double EPSILON = 2e-2;
+    static constexpr double EPSILON = 1e-4;
     static constexpr double MARGIN = 1e-6;
-    const double mu_s = 0.5;
-    const double mu_k = 0.3;
+    // NOTE: Use h=1e-10 for finite difference because min eps_v=1e-8
+    static constexpr double H = 1e-10;
+    const double mu_s = GENERATE(range(0.0, 1.0, 0.1));
+    const double mu_k = GENERATE(range(0.0, 1.0, 0.1));
     const double eps_v = std::pow(10, GENERATE(range(-8, 0, 1)));
-    double x;
-    // SECTION("x=0") { x = 0; }
-    // SECTION("x=gen") {
-    x = std::pow(10, GENERATE(range(-8, 0, 1)));
-    // }
+    const double x = std::pow(10, GENERATE(range(-8, 0, 1)));
 
     if (x == 1e-8 && eps_v == 1e-8) {
         return;
     }
 
-    CAPTURE(x, eps_v);
+    CAPTURE(x, eps_v, x / eps_v, mu_s, mu_k);
 
     Eigen::Matrix<double, 1, 1> X;
     X << x;
 
     // Check gradient
-
-    Eigen::VectorXd fd_mu1(1);
-    fd::finite_gradient(
-        X,
-        [&](const Eigen::VectorXd& _X) {
-            return smooth_mu(_X[0], mu_s, mu_k, eps_v);
-        },
-        fd_mu1);
-
-    CHECK(
-        smooth_mu_derivative(x, mu_s, mu_k, eps_v)
-        == Catch::Approx(fd_mu1[0]).margin(MARGIN).epsilon(EPSILON));
 
     Eigen::VectorXd fd_f1(1);
     fd::finite_gradient(
@@ -51,7 +37,7 @@ TEST_CASE("Smooth mu", "[friction][mollifier][mu]")
         [&](const Eigen::VectorXd& _X) {
             return smooth_mu_f0(_X[0], mu_s, mu_k, eps_v);
         },
-        fd_f1);
+        fd_f1, fd::AccuracyOrder::SECOND, H);
 
     CHECK(
         smooth_mu_f1(x, mu_s, mu_k, eps_v)
@@ -66,13 +52,25 @@ TEST_CASE("Smooth mu", "[friction][mollifier][mu]")
         return;
     }
 
+    Eigen::VectorXd fd_mu1(1);
+    fd::finite_gradient(
+        X,
+        [&](const Eigen::VectorXd& _X) {
+            return smooth_mu(_X[0], mu_s, mu_k, eps_v);
+        },
+        fd_mu1, fd::AccuracyOrder::SECOND, H);
+
+    CHECK(
+        smooth_mu_derivative(x, mu_s, mu_k, eps_v)
+        == Catch::Approx(fd_mu1[0]).margin(MARGIN).epsilon(EPSILON));
+
     Eigen::VectorXd fd_f2(1);
     fd::finite_gradient(
         X,
         [&](const Eigen::VectorXd& _X) {
             return smooth_mu_f1(_X[0], mu_s, mu_k, eps_v);
         },
-        fd_f2);
+        fd_f2, fd::AccuracyOrder::SECOND, H);
 
     CHECK(
         smooth_mu_f2(x, mu_s, mu_k, eps_v)
