21edcc230fd0e05caca3893c3c5b7bc7f92e93c0
Zachary Ferguson <zy.fergus@gmail.com>
2025-08-25 21:52:50 -0400

    Update scalable CCD package version in CMake configuration

--- cmake/recipes/scalable_ccd.cmake
+++ cmake/recipes/scalable_ccd.cmake
@@ -9,7 +9,7 @@ message(STATUS "Third-party: creating target 'scalable_ccd::scalable_ccd'")
 set(SCALABLE_CCD_WITH_CUDA ${IPC_TOOLKIT_WITH_CUDA} CACHE BOOL "Enable CUDA CCD" FORCE)
 
 include(CPM)
-CPMAddPackage("gh:continuous-collision-detection/scalable-ccd#2c82b9ca43fba30b85f7e9aa83283464b1bb7843")
+CPMAddPackage("gh:continuous-collision-detection/scalable-ccd#4fa806f533b19132e696a2dddeab16537025b5f9")
 
 # Folder name for IDE
 set_target_properties(scalable_ccd PROPERTIES FOLDER "ThirdParty")
