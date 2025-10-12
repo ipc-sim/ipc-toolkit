ed27fab86eecf7314ac1f7d2b97007d6b4eddf1f
Zachary Ferguson <zy.fergus@gmail.com>
2025-10-11 22:10:46 -0400

    Update tinyad.cmake

--- cmake/recipes/tinyad.cmake
+++ cmake/recipes/tinyad.cmake
@@ -1,13 +1,17 @@
-# finite-diff (https://github.com/patr-schm/tinyad)
+# TinyAD (https://github.com/patr-schm/tinyad)
 # License: MIT
-if(TARGET TinyAD)
+if(TARGET TinyAD::TinyAD)
     return()
 endif()
 
 message(STATUS "Third-party: creating target 'TinyAD::TinyAD'")
 
+include(Eigen) # TinyAD will find Eigen if Eigen::Eigen3 is not defined
+
 include(CPM)
 CPMAddPackage("gh:patr-schm/tinyad#4b48d1a1a588874556a692a3abbdecd0db4c23e1")
+
+add_library(TinyAD::TinyAD ALIAS TinyAD)
 
 # Folder name for IDE
 set_target_properties(TinyAD PROPERTIES FOLDER "ThirdParty")
